0c29c7f21e316777296ac9aad09752614e77c3e8
Or <or.dicker@gmail.com>
2021-07-11 15:24:22 +0300

    WIP - PID example works

--- src/pid.jl
+++ src/pid.jl
@@ -1,4 +1,4 @@
-using ModelingToolkit
+using ModelingToolkit, DifferentialEquations, Plots
 
 @parameters t
 
@@ -10,28 +10,32 @@ function PID_factory(;name, kp=1.0, ki=0.0, kd=0.0)
     eqs = [
         D(Ie) ~ e
         D(e) ~ de
-        D(u) ~ _ki*Ie+_kp*e+_kd*de
+        u ~ _ki*Ie+_kp*e+_kd*de
     ]
-    ODESystem(eqs;name, defaults=[u=>0.0,
-                                  e=>0.0, de=>0.0, Ie=>0.0,
-                                  _kp=>kp, _ki=>ki, _kd=>kd])
+    ODESystem(eqs, t ;name, defaults=[u=>0.0,
+                                      e=>0.0, de=>0.0, Ie=>0.0,
+                                      _kp=>kp, _ki=>ki, _kd=>kd])
 end
 
 function test()
-    @named pid1 = Gimbal.PID_factory(kp=2.2,ki=0.1,kd=0.3)
+    @named pid1 = Gimbal.PID_factory(kp=350.0,ki=300.0,kd=50.0)
     @named p = Gimbal.plant()
-    connections = [ pid1.e ~ p.x
-                    p.F ~ sin(t)]
-    @named connected = ODESystem(connections; systems=[pid1,p])
-    @show equations(connected)
-    connected |> structural_simplify 
+    connections = [pid1.e ~ 1/(1+exp(-100*(t-1)))-p.x
+                   p.F ~ pid1.u]
+    @named connected = ODESystem(connections ; systems=[pid1,p])
+    #@show equations(connected)
+    sys = structural_simplify(connected)
+    prob = ODEProblem(sys,[],(0.0,3.0))
+    sol = solve(prob, ImplicitEuler(),adaptive=false, dt=4e-4)
+    plot(sol, vars = [p.x])#, pid1.e, pid1.u])
 end
 
 function plant(;name)
-    @variables t F(t) x(t)
+    @variables t F(t) x(t) dx(t)
     D = Differential(t)  
     eqs = [
-        D(x) ~ F
+        D(x) ~ dx
+        D(dx) ~ F - 10*dx - 20*x
     ]
-    ODESystem(eqs;name, defaults=[x=>0.0])
+    ODESystem(eqs, t;name, defaults=[x=>0.0, dx=>0.0])
 end
