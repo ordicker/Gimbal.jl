c992019ed1dcc730fb7f64d38ac2bef25874418c
Or <or.dicker@gmail.com>
2021-07-11 17:45:54 +0300

    check point with lead_lag_factory

--- src/pid.jl
+++ src/pid.jl
@@ -17,3 +17,16 @@ function PID_factory(;name, kp=1.0, ki=0.0, kd=0.0)
                                       _kp=>kp, _ki=>ki, _kd=>kd])
 end
 
+function lead_lag_factory(zero, pole;name)
+    @variables t ein(t) din(t) out(t) dout(t)
+    @parameters _zero _pole
+    D = Differential(t)
+    eqs = [
+        D(ein) ~ din
+        D(out) ~ dout
+        _pole*dout + out ~ _zero*din + ein
+    ]
+    ODESystem(eqs, t;name, defaults=[out=>0.0,dout=>0.0,
+                                     ein=>0.0, din=>0.0,
+                                     _zero=>zero, _pole=>pole])
+end
