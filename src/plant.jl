a12924e68fe93db346c49570acb5ddd26478b561
Or <or.dicker@gmail.com>
2021-09-01 11:28:07 +0300

    fix the stab_error function; fix plant params

--- src/plant.jl
+++ src/plant.jl
@@ -1,20 +1,21 @@
 
 function gimbal_plant(;name, J=1.0, k_s=1.0, k_v=1.0, ω_brk=1.0, T_c=1.0,T_brk=1.0)
     @variables t T(t) ω_rel(t) θ_rel(t) ω_body(t) ω(t)
-    @parameters _J _k_s _k_v _ω_brk _T_c
+    @parameters _J _k_s _k_v _ω_brk _T_c _ω_brk _T_c _T_brk
     D = Differential(t)
     eqs = [
         D(θ_rel) ~ ω_rel
         ω ~ ω_rel + ω_body
         D(ω) ~ (T-_k_s*θ_rel-_k_v*ω_rel
                 -_T_c*tanh(10*ω_rel/_ω_brk)
-                -sqrt(2ℯ)*(T_brk-T_c)*exp(-(ω_rel/(ω_brk*sqrt(2)))^2)*(ω_rel/(ω_brk*sqrt(2)))
+                -sqrt(2ℯ)*(_T_brk-_T_c)*exp(-(ω_rel/(_ω_brk*sqrt(2)))^2)*(ω_rel/(_ω_brk*sqrt(2)))
                 )/_J
     ]
-    ODESystem(eqs, t; name, defaults=[ω=>0.0, ω_body=>0.0, ω_rel=>0.0,
-                                      θ_rel=>0.0, T=>0.0,
-                                      _T_c=>T_c, _ω_brk=>ω_brk,
-                                      _J=>J, _k_s=>k_s, _k_v=>k_v])
+    ODESystem(eqs, t; name, defaults =
+        [ω=>0.0, ω_body=>0.0, ω_rel=>0.0,
+         θ_rel=>0.0, T=>0.0,
+         _T_c=>T_c, _T_brk=> T_brk, _ω_brk=>ω_brk,
+         _J=>J, _k_s=>k_s, _k_v=>k_v])
 end
 
 export gimbal_plant
--- src/uncertainty.jl
+++ src/uncertainty.jl
@@ -1,14 +1,15 @@
 using DiffEqUncertainty, Distributions
 
 
-stab_error(sol,p) = sqrt(mean(abs2,sol[p.ω]))#*last(sol.t)
+stab_error(sol,p) = 1e6*sqrt(mean(abs2,cumsum(sol[p.ω][1:end-1]).*diff(sol.t)))
 
 function temp()
     prob, p, _ = gimbal_conntroller(k_s=Uniform(0.2-0.04,0.2+0.04),
                                     k_v=Uniform(0.545-0.109,0.545+0.109),
                                     ω_brk=Uniform(0.074-0.0148,0.074+0.0148),
                                     T_c=Uniform(0.106-0.0212,0.106+0.0212),
-                                    T_brk=Uniform(0.177-0.0354,0.177+0.0354))
+                                    T_brk=Uniform(0.177-0.0354,0.177+0.0354)
+                                    )
     g(x)=stab_error(x,p)
     expectation(g, prob, prob.u0, prob.p, Koopman(), Rodas5())
     #expectation(g, prob, prob.u0, prob.p, MonteCarlo(), Rodas5(); trajectories = 100)
