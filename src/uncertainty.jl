c40d46622175751a64f3d5cd7cd428935867dd98
Or <or.dicker@gmail.com>
2021-09-13 16:45:55 +0300

    first version of loss function;fix stab_error

--- src/uncertainty.jl
+++ src/uncertainty.jl
@@ -1,18 +1,25 @@
-using DiffEqUncertainty, Distributions
+using DiffEqUncertainty, Distributions, ForwardDiff
 
+stab_error(sol,index) = 1e3*sqrt(mean(abs2,cumsum(sol[index,:])))
 
-stab_error(sol) = 1e6*sqrt(mean(abs2,cumsum(sol[26,1:end-1])))/1000
+#utilities
+indexof(sym,syms) = findfirst(isequal(sym),syms)
 
-function temp()
-    prob, p , _ = gimbal_conntroller(k_s=Uniform(0.2-0.04,0.2+0.04),
-                                     k_v=Uniform(0.545-0.109,0.545+0.109),
-                                     ω_brk=Uniform(0.074-0.0148,0.074+0.0148),
-                                     T_c=Uniform(0.106-0.0212,0.106+0.0212),
-                                     T_brk=Uniform(0.177-0.0354,0.177+0.0354))
+function loss(θ;full=true)
+    Z1,Z2,P2,Z3,P3,Z4 = θ
+    prob, p , sys = gimbal_conntroller(;full,
+                                       Z1, Z2, P2, Z3, P3, Z4,
+                                       k_s=Uniform(0.2-0.04,0.2+0.04),
+                                       k_v=Uniform(0.545-0.109,0.545+0.109),
+                                       ω_brk=Uniform(0.074-0.0148,0.074+0.0148),
+                                       T_c=Uniform(0.106-0.0212,0.106+0.0212),
+                                       T_brk=Uniform(0.177-0.0354,0.177+0.0354))
 
-    expectation(stab_error, prob, prob.u0, prob.p,
-                Koopman(), Rodas5();ireltol=1e-1)
+    index = indexof(p.ω,states(sys))
+    g(x) = stab_error(x,index)
+    expectation(g, prob, prob.u0, prob.p,
+                Koopman(), Rodas5(); ireltol=1e-1)[1]
     #expectation(g, prob, prob.u0, prob.p, MonteCarlo(), Rodas5(); trajectories = 100)
 end
 
-export temp, stab_error
+export loss
