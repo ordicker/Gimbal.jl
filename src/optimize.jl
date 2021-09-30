fc391625ce9f5e0a5137680bde6d02c2091aa227
Or <or.dicker@gmail.com>
2021-09-30 09:14:04 +0300

    WIP - making the optimizer work

--- src/optimize.jl
+++ src/optimize.jl
@@ -7,10 +7,11 @@ end
 
 function gimbal_optimize()
     x₀ = [0.01,0.029,0.16,0.06,0.009,0.01]
-    opt = Opt(:LD_MMA, 3)
+    opt = Opt(:LD_MMA, 6)
     opt.lower_bounds = x₀*0.9
     opt.upper_bounds = x₀*1.1
     opt.xtol_rel = 1e-3
+    #opt.maxtime = 10 #time limit in seconds
     opt.min_objective = loss_nlopt
     (minf,minx,ret) = NLopt.optimize(opt, x₀)
 end
